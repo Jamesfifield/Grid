1272e91dd44fbd1a620ab6d8ff15646b226da2dc
jamesfifield <james.fifield@hotmail.com>
2023-01-15 15:33:15 +1300

    added test

--- src/grid.test.ts
+++ src/grid.test.ts
@@ -1,6 +1,7 @@
 import Grid from "./Grid";
 
 describe("Grid Data Structure", () => {
+  //Test Contructor
   describe("constructor creates and sets correct row and column length", () => {
     const rowSize = 8;
     const columSize = 5;
@@ -25,10 +26,11 @@ describe("Grid Data Structure", () => {
     it("throws an error if colums or row set to n where n = -n", () => {
       expect(() => new Grid(-5, -1)).toThrowError();
     });
-  });
+  }); // END of Contructor Test
 
+  //Tets Fill Method
   describe("Grid fills correctly", () => {
-    it("fills grid with data T", () => {
+    it("fills grid with data T where T == typeof Number", () => {
       const grid = new Grid<number>(3, 3);
       const fillData = 8;
       const testArray = JSON.stringify(new Array(3).fill(fillData));
@@ -37,10 +39,32 @@ describe("Grid Data Structure", () => {
       expect(JSON.stringify(grid.toArray())).toMatch(build);
     });
 
-    it("fills grid with data T", () => {
-      const grid = new Grid<{ data: string }>(3, 3);
-      const fillData = { data: "test" };
-      //expect(JSON.stringify(grid.toArray())).toMatch();
+    it("fills grid with data T where T == typeof string", () => {
+      const grid = new Grid<string>(3, 3);
+      const fillData = "hello";
+      const testArray = JSON.stringify(new Array(3).fill(fillData));
+      const build = [testArray, testArray, testArray].join(",");
+      grid.fill(fillData);
+      expect(JSON.stringify(grid.toArray())).toMatch(build);
     });
-  });
+
+    describe("Grid clones objects", () => {
+      const obj = { id: 1 };
+      const grid = new Grid<typeof obj>(3, 3);
+      grid.fill(obj);
+      const obj2 = grid.getValueAt(2, 2);
+
+      it("clones objects therefore grid[1][1] !=== grid[2][2]", () => {
+        expect(obj2).not.toBe(obj);
+      });
+
+      it("copied object have the same key/values", () => {
+        expect(obj2).toMatchObject(obj);
+      });
+
+      it("access object in random grid position", () => {
+        expect(obj2.id).toEqual(obj.id);
+      });
+    });
+  }); //END of Fill Method
 });
